ab14d364c967895c5cafa6fea38726bf1db7b713
Fredrik Jadebeck <fredrik.jadebeck@aixigo.de>
2017-03-02 17:21:17 +0100

    Added all xml escape sequences where necessary.

--- audit.js
+++ audit.js
@@ -284,7 +284,7 @@ function resultCallback(err, pkg) {
 	   console.log("[" + actualAudits + "/" + expectedAudits + "] " + colors.bold.red(pkgName + " " + versionString + "  [VULNERABLE]") + "   ");
            JUnit['testsuite'].push({name: 'testcase', attrs: {name: pkg.name}, children: [{
               name: 'failure', text: `Found ${pkg['vulnerability-matches']} vulnerabilitites. See lines below for details:\n
-              ${JSON.stringify(pkg['vulnerabilities']).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+              ${JSON.stringify(pkg['vulnerabilities']).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g,	'&quot;').replace(/'/g,	'&apos;')
                }\n\n`, attrs: {message:pkg['vulnerability-matches']}}]});
 	}
 	else {
