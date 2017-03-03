16b8f8261c30f44a416f581232c7b8a7848e9d4d
Fredrik Jadebeck <fredrik.jadebeck@aixigo.de>
2017-03-03 13:23:24 +0100

    Formatted JUnit failure text.

--- audit.js
+++ audit.js
@@ -294,7 +294,7 @@ function resultCallback(err, pkg) {
 	   console.log("[" + actualAudits + "/" + expectedAudits + "] " + colors.bold.red(pkgName + " " + versionString + "  [VULNERABLE]") + "   ");
            JUnit['testsuite'].push({name: 'testcase', attrs: {name: pkg.name}, children: [{
               name: 'failure', text: `Details:\n
-              ${JSON.stringify(pkg['vulnerabilities']).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')}\n\n`,
+              ${JSON.stringify(pkg['vulnerabilities'], null, 2).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')}\n\n`,
               attrs: {message:`Found ${pkg['vulnerability-matches']} vulnerabilities. See stacktrace for details.`}}]});
 	}
 	else {
