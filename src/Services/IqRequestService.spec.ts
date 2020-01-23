a419103c86d90cf903c4668503792d582671b62f
Jeffry Hesse <jhesse@sonatype.com>
2020-01-23 14:07:04 -0900

    Start of more IQ Unit Tests

--- src/Services/IqRequestService.spec.ts
+++ src/Services/IqRequestService.spec.ts
@@ -42,4 +42,38 @@ describe("IQRequestService", () => {
     return expect(requestService.getApplicationInternalId()).to.eventually.be
       .rejected;
   });
+
+  it("should have it's internal ID API request accepted when the IQ Server is not down", async () => {
+
+    let response = {
+      statusCode: 200,
+      body: {
+        "applications": [
+          {
+            "id": "4bb67dcfc86344e3a483832f8c496419",
+            "publicId": "testapp",
+            "name": "TestApp",
+            "organizationId": "bb41817bd3e2403a8a52fe8bcd8fe25a",
+            "contactUserName": "NewAppContact",
+            "applicationTags": [
+              {
+                "id": "9beee80c6fc148dfa51e8b0359ee4d4e",
+                "tagId": "cfea8fa79df64283bd64e5b6b624ba48",
+                "applicationId": "4bb67dcfc86344e3a483832f8c496419"
+              }
+            ]
+          }
+        ]
+      }
+    }
+
+    let stage = "build"
+    const scope = nock("http://testlocation:8070")
+      .get(`/api/v2/applications?publicId=testapp`)
+      .reply(response.statusCode, response.body);
+
+    const requestService = new IqRequestService("admin", "admin123", "http://testlocation:8070", "testapp", stage, 300);
+
+    return expect(requestService.getApplicationInternalId()).to.eventually.equal("4bb67dcfc86344e3a483832f8c496419");
+  });
 });
