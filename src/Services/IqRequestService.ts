50654b78d6d816dbb049d90f7b888124fc66f5b0
Jeffry Hesse <jhesse@sonatype.com>
2020-01-23 09:26:22 -0900

    fix: add logging

--- src/Services/IqRequestService.ts
+++ src/Services/IqRequestService.ts
@@ -15,6 +15,7 @@
  */
 import fetch from 'node-fetch';
 import { RequestHelpers } from './RequestHelpers';
+import { logMessage, DEBUG } from '../Application/Logger/Logger';
 
 const APPLICATION_INTERNAL_ID_ENDPOINT = '/api/v2/applications?publicId=';
 
@@ -79,24 +80,35 @@ export class IqRequestService {
   }
 
   public async asyncPollForResults(url: string, errorHandler: (error: any) => any, pollingFinished: (body: any) => any) {
-    // https://www.youtube.com/watch?v=Pubd-spHN-0
-    const response = await fetch(
-      this.getURLOrMerge(url).href, { 
-        method: 'get', 
-        headers: [this.getBasicAuth(), RequestHelpers.getUserAgent()]
-      });
-    const body = response.ok;
-    // TODO: right now I think we cover 500s and 400s the same and we'd continue polling as a result. We should likely switch
-    // to checking explicitly for a 404 and if we get a 500/401 or other throw an error
-    if (!body) {
-      this.timeoutAttempts += 1;
-      if (this.timeoutAttempts > this.timeout) {
-        errorHandler({message: "Polling attempts exceeded, please either provide a higher limit via the command line using the timeout flag, or re-examine your project and logs to see if another error happened"})
+    logMessage(url, DEBUG);
+    let mergeUrl: URL;
+    try {
+      mergeUrl = this.getURLOrMerge(url);
+
+      // https://www.youtube.com/watch?v=Pubd-spHN-0
+      const response = await fetch(
+        mergeUrl.href, { 
+          method: 'get', 
+          headers: [this.getBasicAuth(), RequestHelpers.getUserAgent()]
+        });
+  
+      logMessage("response", DEBUG, { response: response });
+      const body = response.ok;
+      // TODO: right now I think we cover 500s and 400s the same and we'd continue polling as a result. We should likely switch
+      // to checking explicitly for a 404 and if we get a 500/401 or other throw an error
+      if (!body) {
+        this.timeoutAttempts += 1;
+        if (this.timeoutAttempts > this.timeout) {
+          errorHandler({message: "Polling attempts exceeded, please either provide a higher limit via the command line using the timeout flag, or re-examine your project and logs to see if another error happened"})
+        }
+        setTimeout(() => this.asyncPollForResults(url, errorHandler, pollingFinished), 1000);
+      } else {
+        let json = await response.json();
+        pollingFinished(json);
       }
-      setTimeout(() => this.asyncPollForResults(url, errorHandler, pollingFinished), 1000);
-    } else {
-      let json = await response.json();
-      pollingFinished(json);
+    }
+    catch (e) {
+      errorHandler({title: e.message});
     }
   }
 
@@ -104,7 +116,8 @@ export class IqRequestService {
     try {
       return new URL(url);
     } catch (e) {
-      if (url[0] === '/') {
+      logMessage(e.title, DEBUG, { message: e.message });
+      if (this.host.endsWith('/')) {
         return new URL(this.host.concat(url)); 
       }
       return new URL(this.host.concat('/' + url)); 
