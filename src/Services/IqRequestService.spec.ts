42e2a6c76aec6b269ed860de5be3648e3851761b
ajurgenson55 <ajurgenson@sonatype.com>
2020-02-03 17:09:50 -0500

    init one liner for iq request service

--- src/Services/IqRequestService.spec.ts
+++ src/Services/IqRequestService.spec.ts
@@ -25,7 +25,8 @@ describe("IQRequestService", () => {
     const scope = nock("http://testlocation:8070")
       .post(`/api/v2/scan/applications/${internalId}/sources/auditjs?stageId=${stage}`)
       .replyWithError("you messed up!");
-    const requestService = new IqRequestService("admin", "admin123", "http://testlocation:8070", "testapp", stage, 300);
+    const requestService = await new IqRequestService("admin", "admin123", "http://testlocation:8070", "testapp", stage, 300).init();
+    console.log(requestService);
     const coords = [new Coordinates("commander", "2.12.2", "@types")];
 
     return expect(requestService.submitToThirdPartyAPI(coords)).to.eventually.be
