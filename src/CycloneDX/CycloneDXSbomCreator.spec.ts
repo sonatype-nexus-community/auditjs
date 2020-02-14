7e9b9f08e1efb04264e9a336cd5bc091bb826086
Jeffry Hesse <jhesse@sonatype.com>
2020-02-14 09:43:01 -0900

    Include scoped dep in test

--- src/CycloneDX/CycloneDXSbomCreator.spec.ts
+++ src/CycloneDX/CycloneDXSbomCreator.spec.ts
@@ -42,14 +42,14 @@ const object = {
         }
       }
     },
-    'testdependency3': {
-      'name': 'testdependency3',
+    '@scope/testdependency3': {
+      'name': '@scope/testdependency3',
       'version': '1.0.2'
     }
   }
 }
 
-const expectedResponse = `<?xml version="1.0" encoding="utf-8"?><bom xmlns="http://cyclonedx.org/schema/bom/1.1" version="1"><components><component type="library" bom-ref="pkg:npm/testdependency@1.0.1"><name>testdependency</name><version>1.0.1</version><description/><purl>pkg:npm/testdependency@1.0.1</purl></component><component type="library" bom-ref="pkg:npm/testdependency2@1.0.2"><name>testdependency2</name><version>1.0.2</version><description/><purl>pkg:npm/testdependency2@1.0.2</purl></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.0"><name>testdependency</name><version>1.0.0</version><description/><purl>pkg:npm/testdependency@1.0.0</purl></component><component type="library" bom-ref="pkg:npm/testdependency3@1.0.2"><name>testdependency3</name><version>1.0.2</version><description/><purl>pkg:npm/testdependency3@1.0.2</purl></component></components></bom>`;
+const expectedResponse = `<?xml version="1.0" encoding="utf-8"?><bom xmlns="http://cyclonedx.org/schema/bom/1.1" version="1"><components><component type="library" bom-ref="pkg:npm/testdependency@1.0.1"><name>testdependency</name><version>1.0.1</version><description/><purl>pkg:npm/testdependency@1.0.1</purl></component><component type="library" bom-ref="pkg:npm/testdependency2@1.0.2"><name>testdependency2</name><version>1.0.2</version><description/><purl>pkg:npm/testdependency2@1.0.2</purl></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.0"><name>testdependency</name><version>1.0.0</version><description/><purl>pkg:npm/testdependency@1.0.0</purl></component><component type="library" bom-ref="pkg:npm/%40scope/testdependency3@1.0.2"><group>@scope</group><name>testdependency3</name><version>1.0.2</version><description/><purl>pkg:npm/%40scope/testdependency3@1.0.2</purl></component></components></bom>`;
 
 describe("CycloneDXSbomCreator", async () => {
   it("should create an sbom given a minimal valid object", async () => {
