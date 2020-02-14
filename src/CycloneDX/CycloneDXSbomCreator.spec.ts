52ac44c1084bd94d5fd24b87b4b9f49f7a1c98a2
Jeffry Hesse <jhesse@sonatype.com>
2020-02-14 12:49:54 -0900

    Better description

--- src/CycloneDX/CycloneDXSbomCreator.spec.ts
+++ src/CycloneDX/CycloneDXSbomCreator.spec.ts
@@ -16,7 +16,7 @@
 import { CycloneDXSbomCreator } from './CycloneDXSbomCreator';
 import expect from '../Tests/TestHelper';
 
-// Test object with circular dependency
+// Test object with circular dependency, scoped dependency, dependency with dependency
 const object = {
   'name': 'testproject',
   'version': '1.0.0',
@@ -52,7 +52,7 @@ const object = {
 const expectedResponse = `<?xml version="1.0" encoding="utf-8"?><bom xmlns="http://cyclonedx.org/schema/bom/1.1" version="1"><components><component type="library" bom-ref="pkg:npm/testdependency@1.0.1"><name>testdependency</name><version>1.0.1</version><description/><purl>pkg:npm/testdependency@1.0.1</purl></component><component type="library" bom-ref="pkg:npm/testdependency2@1.0.2"><name>testdependency2</name><version>1.0.2</version><description/><purl>pkg:npm/testdependency2@1.0.2</purl></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.0"><name>testdependency</name><version>1.0.0</version><description/><purl>pkg:npm/testdependency@1.0.0</purl></component><component type="library" bom-ref="pkg:npm/%40scope/testdependency3@1.0.2"><group>@scope</group><name>testdependency3</name><version>1.0.2</version><description/><purl>pkg:npm/%40scope/testdependency3@1.0.2</purl></component></components></bom>`;
 
 describe("CycloneDXSbomCreator", async () => {
-  it("should create an sbom given a minimal valid object", async () => {
+  it("should create an sbom string given a minimal valid object", async () => {
     let sbomCreator = new CycloneDXSbomCreator(process.cwd());
 
     let string = await sbomCreator.createBom(object);
