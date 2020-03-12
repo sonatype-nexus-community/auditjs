c113741de9dbbcb87a8a509a24a047bb85c34e51
Tim Levett <tim.levett@gmail.com>
2020-03-12 11:04:17 -0500

    fix: (bug) Fixed NPE in logger if meta is not passed in (#183)
    💥

--- src/Application/Logger/Logger.ts
+++ src/Application/Logger/Logger.ts
@@ -25,8 +25,8 @@ const logPath = join(homedir(), '.ossindex');
 
 const logPathFile = join(logPath, '.auditjs.combined.log');
 
-addLayout('json', function(config) {
-  return function(logEvent) {
+addLayout('json', function (config) {
+  return function (logEvent) {
     return JSON.stringify(logEvent) + config.separator;
   };
 });
@@ -66,7 +66,7 @@ export const logMessage = (message: string, level: string, ...meta: any) => {
   if (level == DEBUG) {
     logger.debug(message, ...meta);
   } else if (level == ERROR) {
-    if (meta[0].stack) {
+    if (meta && meta[0] && meta[0].stack) {
       logger.error(message, meta[0].stack);
     } else {
       logger.error(message, ...meta);
