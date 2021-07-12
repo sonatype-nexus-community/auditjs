3df29e744d9ac484f0cf79a7f26a0bc40489f87c
Dan Rollo <danrollo@gmail.com>
2021-07-12 16:13:39 -0400

    remove obsolete TODO comments

--- src/CycloneDX/CycloneDXSbomCreator.spec.ts
+++ src/CycloneDX/CycloneDXSbomCreator.spec.ts
@@ -62,23 +62,19 @@ const serialNumberToReplace = 'serialNumberToReplace';
 const metadataTimestampToReplace = 'metadataTimestampToReplace';
 
 const expectedResponseTemplate =
-  // @todo OK to use https for bom xmlns?
   `<?xml version="1.0" encoding="utf-8"?><bom xmlns="https://cyclonedx.org/schema/bom/` +
   bomSchemaVersion +
   `" serialNumber="` +
   serialNumberToReplace +
-  // @todo Should `version` be the same value as bomSchemaVersion?
   `" version="1"><metadata><timestamp>` +
   metadataTimestampToReplace +
   `</timestamp><component type="library" bom-ref="pkg:npm/testproject@1.0.0"><group/><name>testproject</name><version>1.0.0</version><purl>pkg:npm/testproject@1.0.0</purl></component></metadata><components><component type="library" bom-ref="pkg:npm/testdependency@1.0.1"><name>testdependency</name><version>1.0.1</version><purl>pkg:npm/testdependency@1.0.1</purl><description/><externalReferences><reference type="issue-tracker"><url>git+ssh://git@github.com/slackhq/csp-html-webpack-plugin.git</url></reference></externalReferences></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.3"><name>testdependency</name><version>1.0.3</version><purl>pkg:npm/testdependency@1.0.3</purl><description/></component><component type="library" bom-ref="pkg:npm/testdependency2@1.0.2"><name>testdependency2</name><version>1.0.2</version><purl>pkg:npm/testdependency2@1.0.2</purl><description/></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.0"><name>testdependency</name><version>1.0.0</version><purl>pkg:npm/testdependency@1.0.0</purl><description/></component><component type="library" bom-ref="pkg:npm/%40scope/testdependency3@1.0.2"><group>@scope</group><name>testdependency3</name><version>1.0.2</version><purl>pkg:npm/%40scope/testdependency3@1.0.2</purl><description/></component></components><dependencies><dependency ref="pkg:npm/testproject@1.0.0"><dependency ref="pkg:npm/testdependency@1.0.1"/><dependency ref="pkg:npm/testdependency2@1.0.2"/><dependency ref="pkg:npm/%40scope/testdependency3@1.0.2"/></dependency><dependency ref="pkg:npm/testdependency@1.0.1"><dependency ref="pkg:npm/testdependency@1.0.3"/></dependency><dependency ref="pkg:npm/testdependency@1.0.3"/><dependency ref="pkg:npm/testdependency2@1.0.2"><dependency ref="pkg:npm/testdependency@1.0.0"/></dependency><dependency ref="pkg:npm/testdependency@1.0.0"/><dependency ref="pkg:npm/%40scope/testdependency3@1.0.2"/></dependencies></bom>`;
 
 const expectedSpartanResponseTemplate =
-  // @todo OK to use https for bom xmlns?
   `<?xml version="1.0" encoding="utf-8"?><bom xmlns="https://cyclonedx.org/schema/bom/` +
   bomSchemaVersion +
   `" serialNumber="` +
   serialNumberToReplace +
-  // @todo Should `version` be the same value as bomSchemaVersion?
   `" version="1"><metadata><timestamp>` +
   metadataTimestampToReplace +
   `</timestamp><component type="library" bom-ref="pkg:npm/testproject@1.0.0"><group/><name>testproject</name><version>1.0.0</version><purl>pkg:npm/testproject@1.0.0</purl></component></metadata><components><component type="library" bom-ref="pkg:npm/testdependency@1.0.1"><name>testdependency</name><version>1.0.1</version><purl>pkg:npm/testdependency@1.0.1</purl></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.3"><name>testdependency</name><version>1.0.3</version><purl>pkg:npm/testdependency@1.0.3</purl></component><component type="library" bom-ref="pkg:npm/testdependency2@1.0.2"><name>testdependency2</name><version>1.0.2</version><purl>pkg:npm/testdependency2@1.0.2</purl></component><component type="library" bom-ref="pkg:npm/testdependency@1.0.0"><name>testdependency</name><version>1.0.0</version><purl>pkg:npm/testdependency@1.0.0</purl></component><component type="library" bom-ref="pkg:npm/%40scope/testdependency3@1.0.2"><group>@scope</group><name>testdependency3</name><version>1.0.2</version><purl>pkg:npm/%40scope/testdependency3@1.0.2</purl></component></components><dependencies><dependency ref="pkg:npm/testproject@1.0.0"><dependency ref="pkg:npm/testdependency@1.0.1"/><dependency ref="pkg:npm/testdependency2@1.0.2"/><dependency ref="pkg:npm/%40scope/testdependency3@1.0.2"/></dependency><dependency ref="pkg:npm/testdependency@1.0.1"><dependency ref="pkg:npm/testdependency@1.0.3"/></dependency><dependency ref="pkg:npm/testdependency@1.0.3"/><dependency ref="pkg:npm/testdependency2@1.0.2"><dependency ref="pkg:npm/testdependency@1.0.0"/></dependency><dependency ref="pkg:npm/testdependency@1.0.0"/><dependency ref="pkg:npm/%40scope/testdependency3@1.0.2"/></dependencies></bom>`;
